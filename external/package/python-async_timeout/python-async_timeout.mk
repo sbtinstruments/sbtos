4feadf2ccc346e678b3b3c0f042e13e0f712bb98
Frederik Aalund <fpa@sbtaqua.com>
2017-06-08 14:37:49 +0200

    Upgraded python-async_timeout to 1.2.1.

--- external/package/python-async_timeout/python-async_timeout.mk
+++ external/package/python-async_timeout/python-async_timeout.mk
@@ -4,10 +4,8 @@
 #
 ################################################################################
 
-PYTHON_ASYNC_TIMEOUT_VERSION = 5cc2db723d082494ca5d008f21734fd7c9ff7423
-PYTHON_ASYNC_TIMEOUT_SITE = $(call github,frederikaalund,async_timeout,$(PYTHON_ASYNC_TIMEOUT_VERSION))
-#PYTHON_ASYNC_TIMEOUT_VERSION = 1.2.0
-#PYTHON_ASYNC_TIMEOUT_SITE = $(call github,aio-libs,async_timeout,$(PYTHON_ASYNC_TIMEOUT_VERSION))
+PYTHON_ASYNC_TIMEOUT_VERSION = v1.2.1
+PYTHON_ASYNC_TIMEOUT_SITE = $(call github,aio-libs,async_timeout,$(PYTHON_ASYNC_TIMEOUT_VERSION))
 PYTHON_ASYNC_TIMEOUT_SETUP_TYPE = setuptools
 PYTHON_ASYNC_TIMEOUT_LICENSE = Apache-2.0
 PYTHON_ASYNC_TIMEOUT_LICENSE_FILES = LICENSE
