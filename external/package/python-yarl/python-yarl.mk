2f4718d050b20b4e16ca07e2f876f2d8c7d1a5d6
Frederik Aalund <fpa@sbtaqua.com>
2017-06-08 14:47:22 +0200

    Upgraded python-yarl to 0.10.2.

--- external/package/python-yarl/python-yarl.mk
+++ external/package/python-yarl/python-yarl.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_YARL_VERSION = 0.10.0
+PYTHON_YARL_VERSION = 0.10.2
 PYTHON_YARL_SOURCE = yarl-$(PYTHON_YARL_VERSION).tar.gz
-PYTHON_YARL_SITE = https://pypi.python.org/packages/e4/aa/bc97551a2eb0c25711da61e16940decefdcc41b7bb8897b3c24e1623ef74
+PYTHON_YARL_SITE = https://pypi.python.org/packages/83/cd/c9d2c92f12de6bbb8ab025a6a9488d64e75f8650e52232b4718124d28279
 PYTHON_YARL_SETUP_TYPE = setuptools
 PYTHON_YARL_LICENSE = Apache-2.0
 PYTHON_YARL_LICENSE_FILES = LICENSE
