a80d217be86643bec27b9f597506b0dc564ca408
Frederik Aalund <fpa@sbtaqua.com>
2017-06-08 14:41:53 +0200

    Upgraded python-cchardet to 2.1.0.

--- external/package/python-cchardet/python-cchardet.mk
+++ external/package/python-cchardet/python-cchardet.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_CCHARDET_VERSION = 2.0.0
+PYTHON_CCHARDET_VERSION = 2.1.0
 PYTHON_CCHARDET_SOURCE= cchardet-$(PYTHON_CCHARDET_VERSION).tar.gz
-PYTHON_CCHARDET_SITE = https://pypi.python.org/packages/5a/87/6678a0f74397fb08008cb05ed8c7f2a0d97233b037619d6f580d96ef2a23
+PYTHON_CCHARDET_SITE = https://pypi.python.org/packages/07/e0/92e36353d3442163f6090a084c06436c5fab74bba5e7ac997b81e390e025
 PYTHON_CCHARDET_SETUP_TYPE = setuptools
 PYTHON_CCHARDET_LICENSE = Mozilla-1.1 or GPLv2 or LGPLv2.1
 PYTHON_CCHARDET_LICENSE_FILES = COPYING
