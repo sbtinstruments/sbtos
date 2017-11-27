4e394d1f8e64496290091f2f46b3c219ec9a84b5
Frederik Aalund <fpa@sbtaqua.com>
2017-11-27 11:38:52 +0100

    Update external package python-uvloop to 0.9.0

--- external/package/python-uvloop/python-uvloop.mk
+++ external/package/python-uvloop/python-uvloop.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_UVLOOP_VERSION = 0.8.0
+PYTHON_UVLOOP_VERSION = 0.9.0
 PYTHON_UVLOOP_SOURCE = uvloop-$(PYTHON_UVLOOP_VERSION).tar.gz
-PYTHON_UVLOOP_SITE = https://pypi.python.org/packages/30/d4/3a85abecaea42c1ca4d2a23c62fe2b94017370c99abec38f6d70b38d0e9f
+PYTHON_UVLOOP_SITE = https://pypi.python.org/packages/2f/d2/ace6713b6ec2aed8a6d6732dc29613b26ae8c0676b57713f92fda8045579
 PYTHON_UVLOOP_SETUP_TYPE = setuptools
 PYTHON_UVLOOP_LICENSE = Apache-2.0 or MIT
 PYTHON_UVLOOP_LICENSE_FILES = LICENSE-APACHE LICENSE-MIT
