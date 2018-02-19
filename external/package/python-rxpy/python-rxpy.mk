765b8ecc8644c927d80df0599177397e2b079752
Frederik Aalund <fpa@sbtaqua.com>
2018-02-19 10:15:09 +0100

    Update rxpy to track commit 44081a1bba8e897768a22da2ae0580456ff13285 (corresponds to 1.6.1)

--- external/package/python-rxpy/python-rxpy.mk
+++ external/package/python-rxpy/python-rxpy.mk
@@ -4,7 +4,9 @@
 #
 ################################################################################
 
-PYTHON_RXPY_VERSION = v1.5.9
+# The commit id corresponds to 1.6.1.
+# The author didn't provide a tag for this version.
+PYTHON_RXPY_VERSION = 44081a1bba8e897768a22da2ae0580456ff13285
 PYTHON_RXPY_SITE = $(call github,ReactiveX,RxPY,$(PYTHON_RXPY_VERSION))
 PYTHON_RXPY_SETUP_TYPE = setuptools
 PYTHON_RXPY_LICENSE = Apache-2.0
