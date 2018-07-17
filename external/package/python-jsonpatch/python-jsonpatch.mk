b10cad938ee0480a1b8ae517b36e0e519fe46b3c
Frederik Peter Aalund <fpa@sbtinstruments.com>
2018-07-17 14:24:56 +0200

    Add external package python-jsonpatch (version 1.23).

---
+++ external/package/python-jsonpatch/python-jsonpatch.mk
@@ -0,0 +1,13 @@
+################################################################################
+#
+# python-jsonpatch
+#
+################################################################################
+
+PYTHON_JSONPATCH_VERSION = v1.23
+PYTHON_JSONPATCH_SITE = $(call github,stefankoegl,python-json-patch,$(PYTHON_JSONPATCH_VERSION))
+PYTHON_JSONPATCH_SETUP_TYPE = setuptools
+PYTHON_JSONPATCH_LICENSE = BSD
+PYTHON_JSONPATCH_LICENSE_FILES = COPYING
+
+$(eval $(python-package))
