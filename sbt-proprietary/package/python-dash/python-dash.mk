7e0d28fb778fced38e042b2db0691b95b5410d7e
Frederik Peter Aalund <fpa@sbtinstruments.com>
2019-10-03 13:56:03 +0200

    feat: add python-dash

---
+++ sbt-proprietary/package/python-dash/python-dash.mk
@@ -0,0 +1,15 @@
+################################################################################
+#
+# python-dash
+#
+################################################################################
+
+PYTHON_DASH_VERSION = refs/tags/v3.0.0-beta.0
+PYTHON_DASH_SITE = git@github.com:sbtinstruments/dash.git
+PYTHON_DASH_SITE_METHOD = git
+PYTHON_DASH_SETUP_TYPE = setuptools
+PYTHON_DASH_LICENSE = PROPRIETARY
+PYTHON_DASH_REDISTRIBUTE = NO
+
+$(eval $(python-package))
+
