495fa2a6a23d7ed61af20e257149d87736c645c5
Frederik Peter Aalund <fpa@sbtinstruments.com>
2019-10-03 12:27:43 +0200

    feat: add python-maskin

---
+++ sbt-proprietary/package/python-maskin/python-maskin.mk
@@ -0,0 +1,15 @@
+################################################################################
+#
+# python-maskin
+#
+################################################################################
+
+PYTHON_MASKIN_VERSION = refs/tags/v3.0.0-beta.0
+PYTHON_MASKIN_SITE = git@github.com:sbtinstruments/maskin.git
+PYTHON_MASKIN_SITE_METHOD = git
+PYTHON_MASKIN_SETUP_TYPE = setuptools
+PYTHON_MASKIN_LICENSE = PROPRIETARY
+PYTHON_MASKIN_REDISTRIBUTE = NO
+
+$(eval $(python-package))
+
