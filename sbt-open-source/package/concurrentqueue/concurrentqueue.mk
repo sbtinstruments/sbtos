284e0effd7f839218730e2e1a5ca72d21ef33dec
Frederik Peter Aalund <fpa@sbtinstruments.com>
2019-10-03 17:10:51 +0200

    feat: add host-concurrentqueue
    Note that this is a host-only package only used for building.
    It does not have a Config.in or Config.in.host file.

---
+++ sbt-open-source/package/concurrentqueue/concurrentqueue.mk
@@ -0,0 +1,18 @@
+################################################################################
+#
+# concurrentqueue
+#
+################################################################################
+
+CONCURRENTQUEUE_VERSION = ffda5a47ffa8c8f4aa411f933b18509234dd4ab4
+CONCURRENTQUEUE_SITE = https://github.com/cameron314/concurrentqueue.git
+CONCURRENTQUEUE_SITE_METHOD = git
+CONCURRENTQUEUE_LICENSE = PROPRIETARY
+CONCURRENTQUEUE_REDISTRIBUTE = NO
+
+define HOST_CONCURRENTQUEUE_INSTALL_CMDS
+	$(INSTALL) -D -m 0644 $(@D)/concurrentqueue.h $(STAGING_DIR)/usr/include/
+	$(INSTALL) -D -m 0644 $(@D)/blockingconcurrentqueue.h $(STAGING_DIR)/usr/include/
+endef
+
+$(eval $(host-generic-package))
