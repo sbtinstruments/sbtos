29f158a155efddfa59f64bc13eed79cd6a2cf77b
Frederik Peter Aalund <fpa@sbtinstruments.com>
2019-10-03 12:37:58 +0200

    feat: add python-cellmate

---
+++ sbt-proprietary/package/python-cellmate/python-cellmate.mk
@@ -0,0 +1,15 @@
+################################################################################
+#
+# python-cellmate
+#
+################################################################################
+
+PYTHON_CELLMATE_VERSION = refs/tags/v1.0.0-beta.0
+PYTHON_CELLMATE_SITE = git@github.com:sbtinstruments/cellmate.git
+PYTHON_CELLMATE_SITE_METHOD = git
+PYTHON_CELLMATE_SETUP_TYPE = setuptools
+PYTHON_CELLMATE_LICENSE = PROPRIETARY
+PYTHON_CELLMATE_REDISTRIBUTE = NO
+
+$(eval $(python-package))
+
