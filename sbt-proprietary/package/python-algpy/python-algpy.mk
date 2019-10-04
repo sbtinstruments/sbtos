2720d763e82f0773a2a7c6592b4847e65a9b03b7
Frederik Peter Aalund <fpa@sbtinstruments.com>
2019-10-04 13:41:28 +0200

    feat: add python-algpy
    Python wrapper around the alg library.

---
+++ sbt-proprietary/package/python-algpy/python-algpy.mk
@@ -0,0 +1,14 @@
+################################################################################
+#
+# alg.py
+#
+################################################################################
+
+PYTHON_ALGPY_VERSION = refs/tags/v2.0.0-beta.2
+PYTHON_ALGPY_SITE = git@github.com:sbtinstruments/alg.py.git
+PYTHON_ALGPY_SITE_METHOD = git
+PYTHON_ALGPY_LICENSE = PROPRIETARY
+PYTHON_ALGPY_REDISTRIBUTE = NO
+PYTHON_ALGPY_DEPENDENCIES = alg host-pybind11 python3
+
+$(eval $(cmake-package))
