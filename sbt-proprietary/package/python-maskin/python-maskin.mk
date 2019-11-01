52cc47cf5488bf226ccf4b02b0de2ca51a6e6be4
Frederik Peter Aalund <fpa@sbtinstruments.com>
2019-11-01 09:37:10 +0100

    feat: add working battery indicator to the Jupiter GUI
    This uses the new community edition of the Material Design Icons font.

--- sbt-proprietary/package/python-maskin/python-maskin.mk
+++ sbt-proprietary/package/python-maskin/python-maskin.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-PYTHON_MASKIN_VERSION = refs/tags/v3.0.0-beta.0
+PYTHON_MASKIN_VERSION = refs/tags/v3.0.0-beta.1
 PYTHON_MASKIN_SITE = git@github.com:sbtinstruments/maskin.git
 PYTHON_MASKIN_SITE_METHOD = git
 PYTHON_MASKIN_SETUP_TYPE = setuptools
