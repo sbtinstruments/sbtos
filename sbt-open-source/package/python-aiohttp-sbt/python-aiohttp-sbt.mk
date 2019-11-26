65fd14d2a0d88afd328b7b0b9968614ff433afbe
Frederik Peter Aalund <fpa@sbtinstruments.com>
2019-11-26 13:21:25 +0100

    feat: change 'python-aiohttp-sbt' to a custom port of the stable 3.6.2 release
    Previously, the package pointed to the newest aiohttp 4.0.0a1 (alpha) release. However, there were
    too many unsolved issues with this release.
    
    Instead, a stable release is the basis now. Few selected alpha/beta features ported back in.

--- sbt-open-source/package/python-aiohttp-sbt/python-aiohttp-sbt.mk
+++ sbt-open-source/package/python-aiohttp-sbt/python-aiohttp-sbt.mk
@@ -4,15 +4,20 @@
 #
 ################################################################################
 
-PYTHON_AIOHTTP_SBT_VERSION = 4.0.0a1
-PYTHON_AIOHTTP_SBT_SOURCE = aiohttp-$(PYTHON_AIOHTTP_SBT_VERSION).tar.gz
-# We use the 'release' archive (as opposed to the 'source' archive) since the
-# 'release' archive contains the auto-generated .c files used for Cython
-# modules.
-PYTHON_AIOHTTP_SBT_SITE = https://github.com/aio-libs/aiohttp/releases/download/v${PYTHON_AIOHTTP_SBT_VERSION}
+PYTHON_AIOHTTP_SBT_VERSION = refs/tags/v3.6.2-sbt.0
+PYTHON_AIOHTTP_SBT_SITE = https://github.com/sbtinstruments/aiohttp.git
+PYTHON_AIOHTTP_SBT_SITE_METHOD = git
+# For the http-parser submodule
+PYTHON_AIOHTTP_SBT_GIT_SUBMODULES = YES
 PYTHON_AIOHTTP_SBT_SETUP_TYPE = setuptools
 PYTHON_AIOHTTP_SBT_LICENSE = Apache-2.0
 PYTHON_AIOHTTP_SBT_LICENSE_FILES = LICENSE.txt
+
+define PYTHON_AIOHTTP_SBT_CYTHONIZE
+	$(MAKE) -C $(@D) cythonize
+endef
+
+PYTHON_AIOHTTP_SBT_PRE_BUILD_HOOKS += PYTHON_AIOHTTP_SBT_CYTHONIZE
 
 $(eval $(python-package))
 
