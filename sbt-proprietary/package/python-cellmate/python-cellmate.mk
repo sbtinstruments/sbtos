d31e4853a34d43da9e08ddf6c5f18c6e81d20a4a
Frederik Aalund <fpa@sbtinstruments.com>
2020-05-14 17:10:51 +0200

    fix(cellmate): fix syntax errors

--- sbt-proprietary/package/python-cellmate/python-cellmate.mk
+++ sbt-proprietary/package/python-cellmate/python-cellmate.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-PYTHON_CELLMATE_VERSION = refs/tags/v5.0.1
+PYTHON_CELLMATE_VERSION = refs/tags/v5.0.2
 PYTHON_CELLMATE_SITE = git@github.com:sbtinstruments/cellmate.git
 PYTHON_CELLMATE_SITE_METHOD = git
 PYTHON_CELLMATE_SETUP_TYPE = setuptools
