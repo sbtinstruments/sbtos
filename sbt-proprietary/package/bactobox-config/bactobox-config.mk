5e3b393750bb5cabc939fc1bda4f5501a42bf014
Frederik Aalund <fpa@sbtinstruments.com>
2020-05-15 14:10:17 +0200

    feat: update messages.mrp based on empirical data
    Fix division by zero bug.

--- sbt-proprietary/package/bactobox-config/bactobox-config.mk
+++ sbt-proprietary/package/bactobox-config/bactobox-config.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-BACTOBOX_CONFIG_VERSION = bactobox-v1.2.0
+BACTOBOX_CONFIG_VERSION = bactobox-v1.3.0
 BACTOBOX_CONFIG_SITE = git@github.com:sbtinstruments/green-mango-config.git
 BACTOBOX_CONFIG_SITE_METHOD = git
 BACTOBOX_CONFIG_LICENSE = PROPRIETARY
--- sbt-proprietary/package/zeus-config/zeus-config.mk
+++ sbt-proprietary/package/zeus-config/zeus-config.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-ZEUS_CONFIG_VERSION = zeus-v1.4.0
+ZEUS_CONFIG_VERSION = zeus-v1.5.0
 ZEUS_CONFIG_SITE = git@github.com:sbtinstruments/green-mango-config.git
 ZEUS_CONFIG_SITE_METHOD = git
 ZEUS_CONFIG_LICENSE = PROPRIETARY
