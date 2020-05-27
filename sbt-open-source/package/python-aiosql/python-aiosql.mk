321ddf14ffa5dc2db0a1677a52981dee4a1bf2e2
Frederik Aalund <fpa@sbtinstruments.com>
2020-05-27 12:03:55 +0200

    fix(python-aiosql): fix build command
    Previously, we assumed that python-aiosql was setuptools-based.
    It turns out, that is not the case. It is poetry-based.
    
    Now, we simply copy the files over.

--- sbt-open-source/package/python-aiosql/python-aiosql.mk
+++ sbt-open-source/package/python-aiosql/python-aiosql.mk
@@ -10,4 +10,14 @@ PYTHON_AIOSQL_SETUP_TYPE = setuptools
 PYTHON_AIOSQL_LICENSE = BSD-2-Clause
 PYTHON_AIOSQL_LICENSE_FILES = LICENSE
 
-$(eval $(python-package))
+# python-aiosql uses "poetry" as the build system. Buildroot doesn't
+# currently have helpers for poetry-based builds. Luckily, python-aiosql
+# is a pure-python library so we can simply copy the relevant files over.
+define PYTHON_AIOSQL_INSTALL_TARGET_CMDS
+	$(INSTALL) -m 755 -d $(TARGET_DIR)/usr/lib/python3.8/site-packages/aiosql
+	$(INSTALL) -m 644 $(@D)/aiosql/*.py $(TARGET_DIR)/usr/lib/python3.8/site-packages/aiosql
+	$(INSTALL) -m 755 -d $(TARGET_DIR)/usr/lib/python3.8/site-packages/aiosql/adapters
+	$(INSTALL) -m 644 $(@D)/aiosql/adapters/*.py $(TARGET_DIR)/usr/lib/python3.8/site-packages/aiosql/adapters
+endef
+
+$(eval $(generic-package))
