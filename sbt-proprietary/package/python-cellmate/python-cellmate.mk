5e07e8930d16920bea2ac7490a32e64213b90f35
Frederik Aalund <fpa@sbtinstruments.com>
2020-05-27 16:16:37 +0200

    fix(cellmate): fix 'cartridge corrupted' error

--- sbt-proprietary/package/python-cellmate/python-cellmate.mk
+++ sbt-proprietary/package/python-cellmate/python-cellmate.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-PYTHON_CELLMATE_VERSION = refs/tags/v5.1.1
+PYTHON_CELLMATE_VERSION = refs/tags/v5.1.2
 PYTHON_CELLMATE_SITE = git@github.com:sbtinstruments/cellmate.git
 PYTHON_CELLMATE_SITE_METHOD = git
 PYTHON_CELLMATE_SETUP_TYPE = setuptools
