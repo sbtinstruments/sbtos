5ed09a4bc969c3a757ac3d41b1cc03b56c90787b
Frederik Aalund <fpa@sbtinstruments.com>
2020-07-09 08:53:50 +0200

    feat(config): update settings to reflect Romer release
     * Update BBPs
     * Update messages.mrp (Zeus only)
     * Set device ECC (Zeus only)

--- sbt-proprietary/package/bactobox-config/bactobox-config.mk
+++ sbt-proprietary/package/bactobox-config/bactobox-config.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-BACTOBOX_CONFIG_VERSION = bactobox-v1.4.0
+BACTOBOX_CONFIG_VERSION = bactobox-v1.4.0-ext.1
 BACTOBOX_CONFIG_SITE = git@github.com:sbtinstruments/green-mango-config.git
 BACTOBOX_CONFIG_SITE_METHOD = git
 BACTOBOX_CONFIG_LICENSE = PROPRIETARY
