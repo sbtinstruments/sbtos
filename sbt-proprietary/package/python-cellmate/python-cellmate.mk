5ec993b3e899eccb40257d5e7b0d1fb81382fdfb
Jonatan Midtgaard <jonatan.midtgaard@gmail.com>
2020-09-25 15:41:26 +0200

    feat: Rework menu on Bactobox

--- sbt-proprietary/package/python-cellmate/python-cellmate.mk
+++ sbt-proprietary/package/python-cellmate/python-cellmate.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-PYTHON_CELLMATE_VERSION = refs/tags/v5.1.3
+PYTHON_CELLMATE_VERSION = refs/tags/v5.2.0
 PYTHON_CELLMATE_SITE = git@github.com:sbtinstruments/cellmate.git
 PYTHON_CELLMATE_SITE_METHOD = git
 PYTHON_CELLMATE_SETUP_TYPE = setuptools
