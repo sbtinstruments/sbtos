4316279e2720bab34beb93656b16a53d1c3375b8
Frederik Peter Aalund <fpa@sbtinstruments.com>
2020-10-02 14:13:28 +0200

    fix(staten): remove unnecessary decimal points from carry-over message

--- sbt-proprietary/package/bactobox-config/bactobox-config.mk
+++ sbt-proprietary/package/bactobox-config/bactobox-config.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-BACTOBOX_CONFIG_VERSION = bactobox-v1.13.1
+BACTOBOX_CONFIG_VERSION = bactobox-v1.13.2
 BACTOBOX_CONFIG_SITE = git@github.com:sbtinstruments/green-mango-config.git
 BACTOBOX_CONFIG_SITE_METHOD = git
 BACTOBOX_CONFIG_LICENSE = PROPRIETARY
