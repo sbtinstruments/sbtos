59b19720596e2c444b655d6e84e401a6a0712a6d
tvi <tvi@sbtinstruments.com>
2020-12-10 15:13:50 +0100

    feat(dash): remove secrets and add button hold control

--- sbt-proprietary/package/python-dash/python-dash.mk
+++ sbt-proprietary/package/python-dash/python-dash.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-PYTHON_DASH_VERSION = refs/tags/v11.0.0
+PYTHON_DASH_VERSION = refs/tags/v12.0.0
 PYTHON_DASH_SITE = git@github.com:sbtinstruments/dash.git
 PYTHON_DASH_SITE_METHOD = git
 PYTHON_DASH_SETUP_TYPE = setuptools
