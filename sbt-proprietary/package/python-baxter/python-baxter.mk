6b615d71f76fd591cfa007ffae5de4d0e837d206
Frederik Peter Aalund <fpa@sbtinstruments.com>
2021-01-22 13:42:00 +0100

    feat: add an early conductivity check
    This also reworks the menu system internals.

--- sbt-proprietary/package/python-baxter/python-baxter.mk
+++ sbt-proprietary/package/python-baxter/python-baxter.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-PYTHON_BAXTER_VERSION = refs/tags/v5.2.2
+PYTHON_BAXTER_VERSION = refs/tags/v5.2.3
 PYTHON_BAXTER_SITE = git@github.com:sbtinstruments/baxter.git
 PYTHON_BAXTER_SITE_METHOD = git
 PYTHON_BAXTER_SETUP_TYPE = setuptools
--- sbt-proprietary/package/python-maskin/python-maskin.mk
+++ sbt-proprietary/package/python-maskin/python-maskin.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-PYTHON_MASKIN_VERSION = refs/tags/v6.1.0
+PYTHON_MASKIN_VERSION = refs/tags/v6.2.0
 PYTHON_MASKIN_SITE = git@github.com:sbtinstruments/maskin.git
 PYTHON_MASKIN_SITE_METHOD = git
 PYTHON_MASKIN_SETUP_TYPE = setuptools
