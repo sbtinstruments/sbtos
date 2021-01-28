63a2f68489821c3b0d571faa1995d90d64687de0
Jonatan Midtgaard <jonatan.midtgaard@gmail.com>
2021-01-28 15:57:48 +0100

    feat: Noise estimate and bugfixes from full-device-test

--- sbt-proprietary/package/python-dash/python-dash.mk
+++ sbt-proprietary/package/python-dash/python-dash.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-PYTHON_DASH_VERSION = refs/tags/v12.6.0
+PYTHON_DASH_VERSION = refs/tags/v12.7.0
 PYTHON_DASH_SITE = git@github.com:sbtinstruments/dash.git
 PYTHON_DASH_SITE_METHOD = git
 PYTHON_DASH_SETUP_TYPE = setuptools
