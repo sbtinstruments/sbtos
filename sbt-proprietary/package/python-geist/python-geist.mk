08cb1129adedf856d70f7713d5c92dbe78d057bc
Frederik Peter Aalund <fpa@sbtinstruments.com>
2021-02-06 11:03:21 +0100

    fix(geist): fix compatibility with recent SWUpdate release

--- sbt-proprietary/package/python-geist/python-geist.mk
+++ sbt-proprietary/package/python-geist/python-geist.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-PYTHON_GEIST_VERSION = refs/tags/v6.8.1
+PYTHON_GEIST_VERSION = refs/tags/v6.9.0
 PYTHON_GEIST_SITE = git@github.com:sbtinstruments/geist.git
 PYTHON_GEIST_SITE_METHOD = git
 PYTHON_GEIST_SETUP_TYPE = setuptools
