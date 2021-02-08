c2ee326baa918c1b6d009042f9ae2e072f205319
Frederik Peter Aalund <fpa@sbtinstruments.com>
2021-02-08 12:14:55 +0100

    fix: fix various UI quirks

--- sbt-proprietary/package/python-mester/python-mester.mk
+++ sbt-proprietary/package/python-mester/python-mester.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-PYTHON_MESTER_VERSION = refs/tags/v3.6.0
+PYTHON_MESTER_VERSION = refs/tags/v3.6.2
 PYTHON_MESTER_SITE = git@github.com:sbtinstruments/mester.git
 PYTHON_MESTER_SITE_METHOD = git
 PYTHON_MESTER_SETUP_TYPE = setuptools
