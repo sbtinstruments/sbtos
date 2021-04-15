e992b805d500392c1e5ad4e4b306433b03ae04f6
Frederik Peter Aalund <fpa@sbtinstruments.com>
2021-04-15 14:54:47 +0200

    fix: fix the 'no action' bug
    we avoid the situation where the flow_rules disabled all actions
    at the same time (leaving the user helpless).

--- sbt-proprietary/package/zeus-config/zeus-config.mk
+++ sbt-proprietary/package/zeus-config/zeus-config.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-ZEUS_CONFIG_VERSION = zeus-v2.13.0
+ZEUS_CONFIG_VERSION = zeus-v2.14.0
 ZEUS_CONFIG_SITE = git@github.com:sbtinstruments/green-mango-config.git
 ZEUS_CONFIG_SITE_METHOD = git
 ZEUS_CONFIG_LICENSE = PROPRIETARY
