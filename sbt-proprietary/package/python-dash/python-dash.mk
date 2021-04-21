fa696d160555d5bd17a6ec288e466d7ff9db8a4f
tvi <tvi@sbtinstruments.com>
2021-04-21 08:32:34 +0200

    feat: rework power off screen and error messages

--- sbt-proprietary/package/python-dash/python-dash.mk
+++ sbt-proprietary/package/python-dash/python-dash.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-PYTHON_DASH_VERSION = refs/tags/v12.11.2
+PYTHON_DASH_VERSION = refs/tags/v12.12.0
 PYTHON_DASH_SITE = git@github.com:sbtinstruments/dash.git
 PYTHON_DASH_SITE_METHOD = git
 PYTHON_DASH_SETUP_TYPE = setuptools
