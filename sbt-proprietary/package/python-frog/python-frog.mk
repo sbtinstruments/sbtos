db005f039d94e50132aa1ea3ed5f7195c1d212d1
tvi <tvi@sbtinstruments.com>
2021-04-26 15:47:20 +0200

    fix(dash): fix history screen
    Revert asyncio API to python3.8 in the archive lobe, so measurement records are
    properly loaded in a thread.

--- sbt-proprietary/package/python-frog/python-frog.mk
+++ sbt-proprietary/package/python-frog/python-frog.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-PYTHON_FROG_VERSION = refs/tags/v1.4.3
+PYTHON_FROG_VERSION = refs/tags/v1.4.4
 PYTHON_FROG_SITE = git@github.com:sbtinstruments/frog.git
 PYTHON_FROG_SITE_METHOD = git
 PYTHON_FROG_SETUP_TYPE = setuptools
