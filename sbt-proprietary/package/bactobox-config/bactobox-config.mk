5d593cd043a8ea3b47e4aa9f8f52388f653574a5
Jonatan Midtgaard <jonatan.midtgaard@gmail.com>
2021-05-07 12:08:47 +0200

    Fix(mester): Inconsistent whitespace in programs

--- sbt-proprietary/package/bactobox-config/bactobox-config.mk
+++ sbt-proprietary/package/bactobox-config/bactobox-config.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-BACTOBOX_CONFIG_VERSION = bactobox-v3.11.0
+BACTOBOX_CONFIG_VERSION = bactobox-v3.11.1
 BACTOBOX_CONFIG_SITE = git@github.com:sbtinstruments/green-mango-config.git
 BACTOBOX_CONFIG_SITE_METHOD = git
 BACTOBOX_CONFIG_LICENSE = PROPRIETARY
