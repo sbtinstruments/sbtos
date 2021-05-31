c2109a5d6c16cf60d692ddbce3aff50aa271d1cc
Jonatan Midtgaard <jonatan.midtgaard@gmail.com>
2021-05-31 15:40:15 +0200

    Fix[baxter-config,bb] Lower noise error threshold

--- sbt-proprietary/package/bactobox-config/bactobox-config.mk
+++ sbt-proprietary/package/bactobox-config/bactobox-config.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-BACTOBOX_CONFIG_VERSION = bactobox-v3.15.1
+BACTOBOX_CONFIG_VERSION = bactobox-v3.15.2
 BACTOBOX_CONFIG_SITE = git@github.com:sbtinstruments/green-mango-config.git
 BACTOBOX_CONFIG_SITE_METHOD = git
 BACTOBOX_CONFIG_LICENSE = PROPRIETARY
