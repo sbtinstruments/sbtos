2ca4e3d978aadce3d1a3a9edf0ebc4aca88627fd
Frederik Aalund <fpa@sbtinstruments.com>
2021-06-23 12:04:40 +0200

    feat: add transition_time_ms to JSON and CSV reports

--- sbt-proprietary/package/python-baxter/python-baxter.mk
+++ sbt-proprietary/package/python-baxter/python-baxter.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-PYTHON_BAXTER_VERSION = refs/tags/v5.12.0
+PYTHON_BAXTER_VERSION = refs/tags/v5.13.0
 PYTHON_BAXTER_SITE = git@github.com:sbtinstruments/baxter.git
 PYTHON_BAXTER_SITE_METHOD = git
 PYTHON_BAXTER_SETUP_TYPE = setuptools
