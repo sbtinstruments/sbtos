6ac8eb2e04a4cf3a4e6f76582545c8c12888c2cf
Frederik Aalund <fpa@sbtinstruments.com>
2021-07-01 18:29:32 +0200

    fix(baxter): fix duration used in the volume estimate

--- sbt-proprietary/package/python-baxter/python-baxter.mk
+++ sbt-proprietary/package/python-baxter/python-baxter.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-PYTHON_BAXTER_VERSION = refs/tags/v5.13.2
+PYTHON_BAXTER_VERSION = refs/tags/v5.13.3
 PYTHON_BAXTER_SITE = git@github.com:sbtinstruments/baxter.git
 PYTHON_BAXTER_SITE_METHOD = git
 PYTHON_BAXTER_SETUP_TYPE = setuptools
