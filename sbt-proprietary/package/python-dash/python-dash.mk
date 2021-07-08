39606041b1b7ca52a3e1c2c405defb3bcc676c7f
Frederik Aalund <fpa@sbtinstruments.com>
2021-07-08 16:39:01 +0200

    feat(dash): disable menu items according to the underlying action

--- sbt-proprietary/package/python-dash/python-dash.mk
+++ sbt-proprietary/package/python-dash/python-dash.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-PYTHON_DASH_VERSION = refs/tags/v12.19.1
+PYTHON_DASH_VERSION = refs/tags/v12.20.0
 PYTHON_DASH_SITE = git@github.com:sbtinstruments/dash.git
 PYTHON_DASH_SITE_METHOD = git
 PYTHON_DASH_SETUP_TYPE = setuptools
