dcc583561411d1b3858eb1b4219c1e5913cdcea4
Frederik Aalund <fpa@sbtinstruments.com>
2021-08-11 15:11:53 +0200

    feat(dash): paginate legal-info screen for performance

--- sbt-proprietary/package/python-dash/python-dash.mk
+++ sbt-proprietary/package/python-dash/python-dash.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-PYTHON_DASH_VERSION = refs/tags/v13.0.0
+PYTHON_DASH_VERSION = refs/tags/v13.1.0
 PYTHON_DASH_SITE = git@github.com:sbtinstruments/dash.git
 PYTHON_DASH_SITE_METHOD = git
 PYTHON_DASH_SETUP_TYPE = setuptools
