60294ab453589072de96bf008a021d7ef0571680
Frederik Aalund <fpa@sbtinstruments.com>
2021-09-21 14:34:52 +0200

    build(swu-client): fix broken package

--- sbt-open-source/package/python-swu-client/python-swu-client.mk
+++ sbt-open-source/package/python-swu-client/python-swu-client.mk
@@ -4,12 +4,10 @@
 #
 ################################################################################
 
-PYTHON_SWU_CLIENT_VERSION = refs/tags/v0.1.0
-PYTHON_SWU_CLIENT_SITE = git@github.com:sbtinstruments/swu-client.git
-PYTHON_SWU_CLIENT_SITE_METHOD = git
+PYTHON_SWU_CLIENT_VERSION = v0.1.1
+PYTHON_SWU_CLIENT_SITE = $(call github,sbtinstruments,swu-client,$(PYTHON_SWU_CLIENT_VERSION))
 PYTHON_SWU_CLIENT_SETUP_TYPE = setuptools
 PYTHON_SWU_CLIENT_LICENSE = MIT
 PYTHON_SWU_CLIENT_LICENSE_FILES = LICENSE
-PYTHON_SWU_CLIENT_REDISTRIBUTE = NO
 
 $(eval $(python-package))
