9dbbfb2cb5bf6b976785a0bf18169b4ca56dc8f0
Frederik Aalund <fpa@sbtinstruments.com>
2021-09-21 09:57:31 +0200

    feat: add auto-update when user inserts USB drive with SWU file

---
+++ sbt-open-source/package/python-swu-client/python-swu-client.mk
@@ -0,0 +1,15 @@
+################################################################################
+#
+# python-swu-client
+#
+################################################################################
+
+PYTHON_SWU_CLIENT_VERSION = refs/tags/v0.1.0
+PYTHON_SWU_CLIENT_SITE = git@github.com:sbtinstruments/swu-client.git
+PYTHON_SWU_CLIENT_SITE_METHOD = git
+PYTHON_SWU_CLIENT_SETUP_TYPE = setuptools
+PYTHON_SWU_CLIENT_LICENSE = MIT
+PYTHON_DAEMON_SBT_LICENSE_FILES = LICENSE
+PYTHON_SWU_CLIENT_REDISTRIBUTE = NO
+
+$(eval $(python-package))
