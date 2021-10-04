07cc187572cd909ca13591fa2f738ff404c7fd29
Jonatan <jonatan.midtgaard@gmail.com>
2021-10-04 15:51:52 +0200

    Feat: added support for sensirion flow sensor

--- sbt-proprietary/package/python-mester/python-mester.mk
+++ sbt-proprietary/package/python-mester/python-mester.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-PYTHON_MESTER_VERSION = refs/tags/v5.1.0
+PYTHON_MESTER_VERSION = refs/tags/v5.2.0
 PYTHON_MESTER_SITE = git@github.com:sbtinstruments/mester.git
 PYTHON_MESTER_SITE_METHOD = git
 PYTHON_MESTER_SETUP_TYPE = setuptools
