71b3f49cf7a193c14fd644d31c71b089e66ecd3d
Frederik Aalund <fpa@sbtinstruments.com>
2021-10-05 16:46:37 +0200

    build: fix typo that caused build to fail

--- sbt-open-source/package/python-anyio/python-anyio.mk
+++ sbt-open-source/package/python-anyio/python-anyio.mk
@@ -5,7 +5,7 @@
 ################################################################################
 
 PYTHON_ANYIO_VERSION = 3.3.2
-PYTHON_ANYIO_SOURCE = multidict-$(PYTHON_ANYIO_VERSION).tar.gz
+PYTHON_ANYIO_SOURCE = anyio-$(PYTHON_ANYIO_VERSION).tar.gz
 PYTHON_ANYIO_SITE = https://files.pythonhosted.org/packages/bf/3f/4d9972a2190759ac2a576c8fa8b64102a11932ccfb10ed928a116e283f5c
 PYTHON_ANYIO_SETUP_TYPE = setuptools
 PYTHON_ANYIO_LICENSE = MIT
