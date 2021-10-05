79bef79957461ff2eef211417e346524f538107e
Frederik Aalund <fpa@sbtinstruments.com>
2021-10-05 16:24:48 +0200

    build: change anyio source to PyPI to fix build

--- sbt-open-source/package/python-anyio/python-anyio.mk
+++ sbt-open-source/package/python-anyio/python-anyio.mk
@@ -5,11 +5,11 @@
 ################################################################################
 
 PYTHON_ANYIO_VERSION = 3.3.2
-PYTHON_ANYIO_SITE = $(call github,agronholm,anyio,$(PYTHON_ANYIO_VERSION))
+PYTHON_ANYIO_SOURCE = multidict-$(PYTHON_ANYIO_VERSION).tar.gz
+PYTHON_ANYIO_SITE = https://files.pythonhosted.org/packages/bf/3f/4d9972a2190759ac2a576c8fa8b64102a11932ccfb10ed928a116e283f5c
 PYTHON_ANYIO_SETUP_TYPE = setuptools
 PYTHON_ANYIO_LICENSE = MIT
 PYTHON_ANYIO_LICENSE_FILES = LICENSE
 PYTHON_ANYIO_DEPENDENCIES = host-python-setuptools-scm
 
 $(eval $(python-package))
-
