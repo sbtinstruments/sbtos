438b16235c3b95811e4f67a44aa294b04b425a6f
Jonatan <jonatan.midtgaard@gmail.com>
2021-10-19 16:37:53 +0200

    Increase pump speed on bactobox, rework flow rate and conductivity errors

--- sbt-proprietary/package/python-baxter/python-baxter.mk
+++ sbt-proprietary/package/python-baxter/python-baxter.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-PYTHON_BAXTER_VERSION = refs/tags/v6.2.0
+PYTHON_BAXTER_VERSION = refs/tags/v6.2.1
 PYTHON_BAXTER_SITE = git@github.com:sbtinstruments/baxter.git
 PYTHON_BAXTER_SITE_METHOD = git
 PYTHON_BAXTER_SETUP_TYPE = setuptools
