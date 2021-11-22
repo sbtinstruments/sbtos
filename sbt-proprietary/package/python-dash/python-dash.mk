5bab2d619dc7ef2c7ced42f2418f28528863af8b
Frederik Aalund <fpa@sbtinstruments.com>
2021-11-22 13:07:04 +0100

    feat: add 'prepare for long-term storage' program to BactoBox

--- sbt-proprietary/package/python-dash/python-dash.mk
+++ sbt-proprietary/package/python-dash/python-dash.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-PYTHON_DASH_VERSION = refs/tags/v14.7.4
+PYTHON_DASH_VERSION = refs/tags/v14.8.0
 PYTHON_DASH_SITE = git@github.com:sbtinstruments/dash.git
 PYTHON_DASH_SITE_METHOD = git
 PYTHON_DASH_SETUP_TYPE = setuptools
