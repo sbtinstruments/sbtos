e634adc627ad205aead37fcd50fffa7be6f5c67d
Frederik Aalund <fpa@sbtinstruments.com>
2021-12-01 13:28:52 +0100

    dash: fix missing 'restart' menu item

--- sbt-proprietary/package/python-dash/python-dash.mk
+++ sbt-proprietary/package/python-dash/python-dash.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-PYTHON_DASH_VERSION = refs/tags/v14.9.0
+PYTHON_DASH_VERSION = refs/tags/v14.9.1
 PYTHON_DASH_SITE = git@github.com:sbtinstruments/dash.git
 PYTHON_DASH_SITE_METHOD = git
 PYTHON_DASH_SETUP_TYPE = setuptools
