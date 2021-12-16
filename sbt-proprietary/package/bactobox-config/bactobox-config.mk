8d88fd71856936e0bb5dd1def3562ff106cdd1c6
Jonatan <jonatan.midtgaard@gmail.com>
2021-12-16 11:09:40 +0100

    fix(bb-config): Fix regressions in measure and measure_reference

--- sbt-proprietary/package/bactobox-config/bactobox-config.mk
+++ sbt-proprietary/package/bactobox-config/bactobox-config.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-BACTOBOX_CONFIG_VERSION = bactobox-v5.11.0
+BACTOBOX_CONFIG_VERSION = bactobox-v5.11.2
 BACTOBOX_CONFIG_SITE = git@github.com:sbtinstruments/green-mango-config.git
 BACTOBOX_CONFIG_SITE_METHOD = git
 BACTOBOX_CONFIG_LICENSE = PROPRIETARY
